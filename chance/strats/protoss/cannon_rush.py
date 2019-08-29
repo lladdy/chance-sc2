51d62b3369c385be5ccf7108a90c5708eebacf27
lladdy <randallboland@bigpond.com>
2019-08-29 11:40:46 +0930

    Fix references

--- chance/strats/protoss/cannon_rush.py
+++ chance/strats/protoss/cannon_rush.py
@@ -1,6 +1,7 @@
 import random
 
 from chance.strats.strat import Strat
+from sc2.constants import *
 
 
 class CannonRush(Strat):
@@ -15,38 +16,40 @@ class CannonRush(Strat):
 
         # Make probes until we have 16 total
         if self._bot.workers.amount < 16 and nexus.is_idle:
-            if self._bot.can_afford(PROBE):
-                self._bot.do(nexus.train(PROBE), subtract_cost=True, subtract_supply=True)
+            if self._bot.can_afford(UnitTypeId.PROBE):
+                self._bot.do(nexus.train(UnitTypeId.PROBE), subtract_cost=True, subtract_supply=True)
 
         # If we have no pylon, build one near starting nexus
-        elif not self._bot.structures(PYLON) and self._bot.already_pending(PYLON) == 0:
-            if self._bot.can_afford(PYLON):
-                await self._bot.build(PYLON, near=nexus)
+        elif not self._bot.structures(UnitTypeId.PYLON) and self._bot.already_pending(UnitTypeId.PYLON) == 0:
+            if self._bot.can_afford(UnitTypeId.PYLON):
+                await self._bot.build(UnitTypeId.PYLON, near=nexus)
 
         # If we have no forge, build one near the pylon that is closest to our starting nexus
-        elif not self._bot.structures(FORGE):
-            pylon_ready = self._bot.structures(PYLON).ready
+        elif not self._bot.structures(UnitTypeId.FORGE):
+            pylon_ready = self._bot.structures(UnitTypeId.PYLON).ready
             if pylon_ready:
-                if self._bot.can_afford(FORGE):
-                    await self._bot.build(FORGE, near=pylon_ready.closest_to(nexus))
+                if self._bot.can_afford(UnitTypeId.FORGE):
+                    await self._bot.build(UnitTypeId.FORGE, near=pylon_ready.closest_to(nexus))
 
         # If we have less than 2 pylons, build one at the enemy base
-        elif self._bot.structures(PYLON).amount < 2:
-            if self._bot.can_afford(PYLON):
+        elif self._bot.structures(UnitTypeId.PYLON).amount < 2:
+            if self._bot.can_afford(UnitTypeId.PYLON):
                 pos = self._bot.enemy_start_locations[0].towards(self._bot.game_info.map_center,
                                                                  random.randrange(8, 15))
-                await self._bot.build(PYLON, near=pos)
+                await self._bot.build(UnitTypeId.PYLON, near=pos)
 
         # If we have no cannons but at least 2 completed pylons, automatically find a placement location and build them near enemy start location
-        elif not self._bot.structures(PHOTONCANNON):
-            if self._bot.structures(PYLON).ready.amount >= 2 and self._bot.can_afford(PHOTONCANNON):
-                pylon = self._bot.structures(PYLON).closer_than(20, self._bot.enemy_start_locations[0]).random
-                await self._bot.build(PHOTONCANNON, near=pylon)
+        elif not self._bot.structures(UnitTypeId.PHOTONCANNON):
+            if self._bot.structures(UnitTypeId.PYLON).ready.amount >= 2 and self._bot.can_afford(
+                UnitTypeId.PHOTONCANNON):
+                pylon = self._bot.structures(UnitTypeId.PYLON).closer_than(20,
+                                                                           self._bot.enemy_start_locations[0]).random
+                await self._bot.build(UnitTypeId.PHOTONCANNON, near=pylon)
 
         # Decide if we should make pylon or cannons, then build them at random location near enemy spawn
-        elif self._bot.can_afford(PYLON) and self._bot.can_afford(PHOTONCANNON):
+        elif self._bot.can_afford(UnitTypeId.PYLON) and self._bot.can_afford(UnitTypeId.PHOTONCANNON):
             # Ensure "fair" decision
             for _ in range(20):
                 pos = self._bot.enemy_start_locations[0].random_on_distance(random.randrange(5, 12))
-                building = PHOTONCANNON if self._bot.state.psionic_matrix.covers(pos) else PYLON
+                building = UnitTypeId.PHOTONCANNON if self._bot.state.psionic_matrix.covers(pos) else UnitTypeId.PYLON
                 await self._bot.build(building, near=pos)
