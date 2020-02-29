33cd5a46eb45c65e6d04523ec09458a9e650530f
lladdy <randallboland@bigpond.com>
2020-03-01 10:26:16 +1030

    Update run ladder game function to include opponent ID

--- chance/__init__.py
+++ chance/__init__.py
@@ -1,15 +1,17 @@
+# Run ladder game
+# This lets python-sc2 connect to a LadderManager game: https://github.com/Cryptyc/Sc2LadderServer
+# Based on: https://github.com/Dentosal/python-sc2/blob/master/examples/run_external.py
 import argparse
 import asyncio
 import logging
 
-import sc2
+import aiohttp
 from sc2.client import Client
-from sc2.sc2process import SC2Process
+
+import sc2
+from sc2.protocol import ConnectionAlreadyClosed
 
 
-# Run ladder game
-# This lets python-sc2 connect to a LadderManager game: https://github.com/Cryptyc/Sc2LadderServer
-# Based on: https://github.com/Dentosal/python-sc2/blob/master/examples/run_external.py
 def run_ladder_game(bot):
     # Load command line arguments
     parser = argparse.ArgumentParser()
@@ -19,9 +21,10 @@ def run_ladder_game(bot):
     parser.add_argument('--ComputerOpponent', type=str, nargs="?", help='Computer opponent')
     parser.add_argument('--ComputerRace', type=str, nargs="?", help='Computer race')
     parser.add_argument('--ComputerDifficulty', type=str, nargs="?", help='Computer difficulty')
+    parser.add_argument('--OpponentId', type=str, nargs="?", help='Opponent ID')
     args, unknown = parser.parse_known_args()
 
-    if args.LadderServer == None:
+    if args.LadderServer is None:
         host = "127.0.0.1"
     else:
         host = args.LadderServer
@@ -29,12 +32,8 @@ def run_ladder_game(bot):
     host_port = args.GamePort
     lan_port = args.StartPort
 
-    # Versus Computer doesn't work yet
-    computer_opponent = False
-    if args.ComputerOpponent:
-        computer_opponent = True
-        computer_race = args.ComputerRace
-        computer_difficulty = args.ComputerDifficulty
+    # Add opponent_id to the bot class (accessed through self.opponent_id)
+    bot.ai.opponent_id = args.OpponentId
 
     # Port config
     ports = [lan_port + p for p in range(1, 6)]
@@ -55,25 +54,24 @@ def run_ladder_game(bot):
 
     # Run it
     result = asyncio.get_event_loop().run_until_complete(g)
-    print(result)
+    return result, args.OpponentId
 
 
-# Modified version of sc2.main._join_game to allow custom host and port.
+# Modified version of sc2.main._join_game to allow custom host and port, and to not spawn an additional sc2process (thanks to alkurbatov for fix)
 async def join_ladder_game(host, port, players, realtime, portconfig, save_replay_as=None, step_time_limit=None,
                            game_time_limit=None):
-    async with SC2Process(host=host, port=port) as server:
-        await server.ping()
-        client = Client(server._ws)
+    ws_url = f"ws://{host}:{port}/sc2api"
+    ws_connection = await aiohttp.ClientSession().ws_connect(ws_url, timeout=120)
 
-        try:
-            result = await sc2.main._play_game(players[0], client, realtime, portconfig, step_time_limit,
-                                               game_time_limit)
-            if save_replay_as is not None:
-                await client.save_replay(save_replay_as)
-            await client.leave()
-            await client.quit()
-        except ConnectionAlreadyClosed:
-            logging.error(f"Connection was closed before the game ended")
-            return None
+    client = Client(ws_connection)
+    try:
+        result = await sc2.main._play_game(players[0], client, realtime, portconfig, step_time_limit, game_time_limit)
+        if save_replay_as is not None:
+            await client.save_replay(save_replay_as)
+    except ConnectionAlreadyClosed:
+        logging.error(f"Connection was closed before the game ended")
+        return None
+    finally:
+        await ws_connection.close()
 
-        return result
+    return result
