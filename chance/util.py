20da24264130ce0b4f24721ab2c6de6e21fe3024
lladdy <randallboland@bigpond.com>
2022-02-08 23:54:56 +1030

    Fix strategy selection

--- chance/util.py
+++ chance/util.py
@@ -3,4 +3,5 @@ import sys
 
 
 def get_strats_from_module(module: str) -> list:
-    return inspect.getmembers(sys.modules[module], inspect.isclass)
+    # gets all classes in the module. Anything that's a class, we assume is a valid strategy.
+    return [module[0] for module in inspect.getmembers(sys.modules[module], inspect.isclass)]
