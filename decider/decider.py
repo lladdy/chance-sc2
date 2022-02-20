5fb88e587d335659c6e5aeac892afc8abbfabe11
lladdy <randallboland@bigpond.com>
2022-02-20 19:11:10 +1030

    Temp fix crash

--- decider/decider.py
+++ decider/decider.py
@@ -113,7 +113,7 @@ class Decider:
         """
         Determines the weighted probabilities for each choice.
         """
-        win_perc = np.divide(chosen_count, won_count, out=np.zeros_like(won_count, dtype=float),
+        win_perc = np.divide(won_count, chosen_count, out=np.zeros_like(won_count, dtype=float),
                              where=chosen_count != 0)
 
         """
